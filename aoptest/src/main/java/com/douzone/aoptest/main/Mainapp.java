df45f1c521ce5596012d1f36c0e481320da0d96b
kangsori <rkdthfl22@naver.com>
2023-02-02 12:44:28 +0900

    commit

---
+++ aoptest/src/main/java/com/douzone/aoptest/main/Mainapp.java
@@ -0,0 +1,18 @@
+package com.douzone.aoptest.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Mainapp {
+
+	public static void main(String[] args) {
+		ApplicationContext ac =
+				new ClassPathXmlApplicationContext("config/applicationContext.xml");
+		
+		ProductService ps = ac.getBean(ProductService.class);
+		ProductVo vo = ps.find("TV");
+		System.out.println(vo);
+
+	}
+
+}
