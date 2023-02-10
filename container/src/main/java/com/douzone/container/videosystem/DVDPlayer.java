161719a6cd58cc7e13d01621d5254b9465fa4ca6
riversound <riversound@localhost>
2023-02-10 15:49:06 +0900

    commit

--- container/src/main/java/com/douzone/container/videosystem/DVDPlayer.java
+++ container/src/main/java/com/douzone/container/videosystem/DVDPlayer.java
@@ -4,10 +4,17 @@ public class DVDPlayer {
 	
 	private DigitalVideoDisc dvd;
 	
+	public DVDPlayer() {
+	}
+	
 	public DVDPlayer(DigitalVideoDisc dvd) {
 		this.dvd = dvd;
 	}
 	
+	public void setDvd(DigitalVideoDisc dvd) {
+		this.dvd = dvd;
+	}
+
 	public String play() {
 		return dvd.play();
 	}
